be7dbb85db6b4b501a113be7b2f61e61c24665ee
Maurycy Blaszczak <mgblaszczak@gmail.com>
2020-06-19 23:04:32 +0200

    Fix try catch

--- main.cpp
+++ main.cpp
@@ -3,8 +3,7 @@
 #include <iostream>
 #include <string>
 
-
-#define EDGES_MAX 1024
+constexpr auto EDGES_MAX = 1024;
 
 int main() {
   std::fstream input_file;
@@ -12,13 +11,22 @@ int main() {
   std::string display;
   int X, Y, Z;
 
-  if (input_file.good() == true) {
+  try {
+    if (!input_file.good()) {
+      throw "File was not opened succesfully";
+    }
+
     std::cout << "Plik otwarty poprawnie" << std::endl;
     // tu operacje na pliku (zapis/odczyt)
     // X - wierzchołek startowy
     // Y - łączna liczba wierzchołków
     // Z - łączna liczba krawędzi
     input_file >> X >> Y >> Z;
+
+    if (EDGES_MAX < Z) {
+      throw "The number of edges it too high. Numer of edges is limited to: 1024";
+    }
+
     Graph* graph = new Graph(false, Z);
     int temp_node_a, temp_node_b, temp_edge_weight;
 
@@ -27,7 +35,7 @@ int main() {
 
     for (int i = 0; i < Z; i++) {
       input_file >> temp_node_a >> temp_node_b >> temp_edge_weight;
-      try {
+      
         if ((temp_node_a > Y) || (temp_node_b > Y)) {
           throw "Nieprawidlowy numer wierzcholka!";
         } else if (temp_node_a == temp_node_b) {
@@ -36,31 +44,29 @@ int main() {
         } else if (temp_edge_weight <= 0) {
           throw "Podano niedodatnia wage krawedzi";
         }
-      } catch (const char *msg) {
+      } 
+      graph->InsertEdge(temp_node_a, temp_node_b, temp_edge_weight, 0);
+    
+      // sprawdzenie poprawnosci wczytanych wartosci
+      // indeks 0 - pierwszy wierzcholek
+      // indeks 1 - drugi wierzcholek
+      // indeks 2 - waga polaczenia
+      graph->print();
+
+      DijkstraAlgorithm(graph, parent, distance, 1);
+
+      PrintShortestPath(5, parent, Z);
+
+      PrintDistances(1, distance, Z);
+
+      TestGraph();
+
+    } catch (const char *msg) {
         std::cerr << msg << std::endl;
         input_file.close();
         return 1;
-      }
-      graph->InsertEdge(temp_node_a, temp_node_b, temp_edge_weight, 0);
     }
-    // sprawdzenie poprawnosci wczytanych wartosci
-    // indeks 0 - pierwszy wierzcholek
-    // indeks 1 - drugi wierzcholek
-    // indeks 2 - waga polaczenia
-    graph->print();
-
-    DijkstraAlgorithm(graph, parent, distance, 1);
-
-    PrintShortestPath(5, parent, Z);
-
-    PrintDistances(1, distance, Z);
-
-    TestGraph();
 
     input_file.close();
-  } else {
-    std::cout << "Plik nie zostal otwarty poprawnie" << std::endl;
-    return 1;
-  }
   return 0;
 }
