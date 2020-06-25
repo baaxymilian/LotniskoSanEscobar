c532be842a094c1305fbc8b8a3b8d23de1d563f7
Imbryczek <bartosz.bryk.lo@gmail.com>
2020-06-25 14:30:28 +0200

    main changes for testing

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "graph.h"
 
 constexpr int edges_max = 1024;
+constexpr int weight_max = 100000;
 
 auto main() -> int
 {
@@ -21,6 +22,9 @@ auto main() -> int
 
 		std::cout << "File was successfully opened" << std::endl;
 
+		std::vector<int> distance;
+		std::vector<int> parent;
+
 		auto starting_node = 0;
 		auto total_nodes = 0;
 		auto total_edges = 0;
@@ -28,7 +32,7 @@ auto main() -> int
 
 		if (edges_max < total_edges)
 		{
-			throw "The number of edges it too high. Numer of edges is limited to: 1024";
+			throw "The number of edges is too high. Numer of edges is limited to: 1024";
 		}
 
 		std::unique_ptr<graph_class> my_graph(new graph_class(false, total_edges));
@@ -36,24 +40,17 @@ auto main() -> int
 		auto temp_node_b = 0;
 		auto temp_edge_weight = 0;
 
-		std::vector<int> parent;
-		std::vector<int> distance;
-
 		for (auto i = 0; i < total_edges; i++)
 		{
 			input_file >> temp_node_a >> temp_node_b >> temp_edge_weight;
 			my_graph->insert_edge(temp_node_a, temp_node_b, temp_edge_weight, false);
 			if ((temp_node_a > total_nodes) || (temp_node_b > total_nodes))
 			{
-				throw "Incorrect number of a node";
+				throw "Incorrect number of a node (nodes should be numbered incrementally)";
 			}
-			if (temp_node_a == temp_node_b)
+			if (weight_max < temp_edge_weight)
 			{
-				throw "You cannot create inbound and outbound highway from the same city";
-			}
-			if (temp_edge_weight <= 0)
-			{
-				throw "Weight must be positive";
+				throw "The edge weight is too big. Maximum weight is limited to: 100 000";
 			}
 		}
 		
@@ -63,26 +60,33 @@ auto main() -> int
 		// indeks 2 - waga polaczenia
 		// 
 
-		std::unique_ptr<network_class> my_network(new network_class(*my_graph, 1));
+		std::unique_ptr<network_class> my_network(new network_class(my_graph, 1));
 		
 		//std::unique_ptr<graph_class> new_graph(new graph_class(*my_graph));
+
+		graph_class* new_graph = new graph_class(*my_graph);
 
 		my_graph->print();
 
 
-		my_network->print_shortest_path(5);
-		my_network->print_shortest_path(2);
-
-		my_network->print_distances(1);
-
-
 		//parent = dijkstra_algorithm(my_graph, parent, distance, 1);
 
-		//print_shortest_path(5, parent, 1);
-		
-		//print_distances(1, distance, total_edges);
+		/*std::cout << new_graph->edges[2]->next->number;
+		std::cout << my_graph->edges[2]->next->number;
 
-		//test_graph();
+		++my_graph->edges[2]->next->number;
+
+		std::cout << new_graph->edges[2]->next->number;
+		std::cout << my_graph->edges[2]->next->number;*/
+
+
+		//new_graph->print();
+
+		//my_network->print_shortest_path(5);
+		my_network->print_shortest_path(5);
+
+		my_network->print_distances();
+
 	}
 	catch (std::string& msg)
 	{
